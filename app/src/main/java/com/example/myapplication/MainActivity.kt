8c257ad3d660c1e7d22e14f37ce2ca4542326378
kim-cookie <kitty1061107@kaist.ac.kr>
2025-07-04 16:24:05 +0900

    category

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val cities = ArrayList<String>() // 도시 이름을 담을 리스트 생성
+
+        // 도시 추가
+        cities.add("seoul")
+        cities.add("london")
+        cities.add("busan")
+        cities.add("tokyo")
+        cities.add("osaka")
+        cities.add("kyoto")
+        cities.add("seattle")
+        cities.add("LA")
+        cities.add("moscow")
+        val cities_Adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, cities) // adapter 생성 (data와 view를 연결해 주는 관리자)
+        val myListView = findViewById<ListView>(R.id.myListView)
+        myListView.adapter = cities_Adapter // 어댑터 붙이기
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
